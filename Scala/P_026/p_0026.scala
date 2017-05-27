bb42367b7b8a755392942988d4ce68b879909968
zmeeks <zmeeks@users.noreply.github.com>
2017-05-27 03:19:22 -0700

    Add files via upload

--- Scala/P_026/p_0026.scala
+++ Scala/P_026/p_0026.scala
@@ -6,10 +6,10 @@ object p_0026 {
 		var max_num = 0
 		var breaker = false
 
-		for(i <- 999 to 1 by -2){
+		for(i <- 999 to 1 by -1){	
 			if(breaker){}
 			else{
-				temp_max = long_div(i,1,0)
+				temp_max = long_div(i,1,0,1,1)
 				if(temp_max > cur_max){
 					cur_max = temp_max
 					max_num = i
@@ -23,22 +23,45 @@ object p_0026 {
 	
 	}
 
-	def long_div(denom:Int, remaind:Int, depth:Int) : Int = {
+	def long_div_helper(denom:Int, remaind:Int, depth_lim:Int) : Int = {
+		var iter_value = 0
 		var remainder = 0
-		var cur_depth = 0
-		if(remaind == 0 || remaind % denom == 0){
-			depth
-		}
-		else if((remaind == 1 && depth > 0)|| (remaind == 10 && depth > 0)|| (remaind == 100 && depth >0)){
-			depth
+		if(depth_lim == 0){
+			remaind
 		}
 		else if(denom < remaind){
 			remainder = remaind % denom
-			cur_depth = long_div(denom,remainder, depth+1)
+			iter_value = long_div_helper(denom,remainder, depth_lim - 1)
+			iter_value
+		}
+		else{
+			remainder = remaind * 10
+			iter_value = long_div_helper(denom,remainder, depth_lim)
+			iter_value
+		}
+
+	}
+
+	def long_div(denom:Int, remaind:Int, depth:Int, iter:Int, iter_val:Int) : Int = {
+		var remainder = 0
+		var cur_depth = 0
+		if(remaind == 0 || remaind % denom == 0){
+			depth - iter + 1
+		}
+		else if((remaind == iter_val && depth >= iter)|| (remaind == 10*iter_val && depth >= iter)|| (remaind == 100*iter_val && depth >= iter)){
+			depth - iter + 1
+		}
+		else if(denom < remaind){
+			remainder = remaind % denom
+			cur_depth = long_div(denom,remainder, depth+1, iter, iter_val)
 			cur_depth
+		}
+		else if(depth > denom + iter){
+			var new_iter_val = long_div_helper(denom, 1, iter+1)
+			long_div(denom,1, 0, iter + 1, new_iter_val)
 		}else{
 			remainder = remaind * 10
-			cur_depth = long_div(denom,remainder, depth)
+			cur_depth = long_div(denom,remainder, depth, iter, iter_val)
 			cur_depth
 		}
 	}
